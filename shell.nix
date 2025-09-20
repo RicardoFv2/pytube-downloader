2d1c51f5e0cf6b522bd1f352fe2728c1d9fe7881
RicardoFv2 <fuentesrf0@gmail.com>
2025-09-20 10:55:08 -0600

    nix fix

--- shell.nix
+++ shell.nix
@@ -1,21 +1,28 @@
 { pkgs ? import <nixpkgs> {} }:
 
+let
+  # Define a custom python310 that overrides sphinx to a compatible version
+  python310WithSphinxFix = pkgs.python310.override {
+    packageOverrides = pythonSelf: pythonSuper: {
+      sphinx = pythonSuper.sphinx.overrideAttrs (oldAttrs: {
+        version = "7.3.11"; # Pinning to a known compatible version
+      });
+    };
+  };
+
+  # Use this custom python environment for the project's packages
+  pythonEnv = python310WithSphinxFix.withPackages (ps: with ps; [
+    customtkinter
+    yt-dlp
+    moviepy
+    requests
+    tqdm
+    packaging
+  ]);
+in
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    # Python interpreter
-    python310
-
-    # Required Python packages
-    (python310.withPackages (ps: with ps; [
-      customtkinter
-      yt-dlp
-      moviepy
-      requests
-      tqdm
-      packaging
-    ]))
-
-    # For video/audio processing
+    pythonEnv
     ffmpeg
   ];
 
